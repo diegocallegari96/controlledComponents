6d8b6243c415c4a401ec2b0141a0fa7a0ac4cc1d
diegocallegari96 <warsindustry@gmail.com>
2023-09-18 16:40:20 +0200

    added some value

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+    const [formData, setFormData] = useState({
+        naam: "",
+        leeftijd: "",
+        opmerkingen: "",
+        nieuwsbrief: false,
+    });
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        console.log({formData});
+    }
+    function handleInputChange(e) {
+        const { name, value, type, checked } = e.target;
+        const newValue = type === "checkbox" ? checked : value;
+
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: newValue,
+        }));
+    }
     return (
         <div>
-            Maak hier jouw formulier!
+            <label htmlFor="details">
+                <h4>Gegevens</h4>
+            <form id="details">
+                Naam:
+                <input
+                    type="text"
+                    name="naam"
+                    value={formData.naam}
+                    onChange={handleInputChange}
+                />
+                <br/>
+                Leeftijd:
+                <input
+                    type="number"
+                    name="leeftijd"
+                    value={formData.leeftijd}
+                    onChange={handleInputChange}
+                />
+            </form>
+            </label>
+            <label htmlFor="review">
+                <h4>Jouw review</h4>
+                <form id="review" onSubmit={handleSubmit}>
+                    Opmerkingen:
+                    <br/>
+                    <textarea
+                        rows="4"
+                        cols="50"
+                        name="opmerkingen"
+                        value={formData.opmerkingen}
+                        onChange={handleInputChange}
+                    />
+                    <br/>
+                    <input
+                        type="checkbox"
+                        name="nieuwsbrief"
+                        value={formData.nieuwsbrief}
+                        onChange={handleInputChange}
+                    /> Ik schrijf me in voor de nieuwsbrief
+                    <br/>
+                    <button
+                        type="submit">
+                        versturen
+                    </button>
+                </form>
+            </label>
         </div>
     );
 }
